863ec4f1725b4889fdd0513f0400c49b054587c6
mhmmedahraz18 <mhmmedahraz@gmail.com>
2026-08-05 17:20:52 +0530

    Fix Vercel deployment configuration

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z$6j68i)#udzjh4-baz0jz8)!_@fge&ea3#g#j76e@x)w=4-0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "moviehub-django.vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -116,6 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
